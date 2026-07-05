4379c771e2b8543dc7b6340d3b95591322ae3acf
langfeld <48012778+langfeld@users.noreply.github.com>
2026-07-05 14:28:48 +0200

    Initialize options flow in Simple Smart Cover configuration

--- custom_components/simple_smart_cover/config_flow.py
+++ custom_components/simple_smart_cover/config_flow.py
@@ -365,6 +365,10 @@ class SimpleSmartCoverConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class SimpleSmartCoverOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle options flow for Simple Smart Cover."""
 
+    def __init__(self) -> None:
+        """Initialize options flow."""
+        super().__init__()
+
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
